6b1a6fa67808476d4d59f6dc2a7b1a0a3141a3b0
230691 Grzelak Filip <230691@edu.p.lodz.pl>
2022-03-07 13:18:46 +0100

    Creating constructor for initialize fifteenPuzzle class and helpful printinh method

--- src/main/java/FifteenPuzzle.java
+++ src/main/java/FifteenPuzzle.java
@@ -1,3 +1,8 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class FifteenPuzzle {
     private int[][] board = new int[4][4];
     private int deepthLevel;
@@ -7,6 +12,20 @@ public class FifteenPuzzle {
         this.board = board.getBoard();
         this.deepthLevel = board.getDeepthLevel() + 1;
         this.createdState = board.getCreatedState() + 1;
+    }
+
+    public FifteenPuzzle(String filename) throws IOException {
+        BufferedReader bf = new BufferedReader(new FileReader(filename));
+        String line;
+        int counter = 0;
+        while((line = bf.readLine()) != null) {
+            String[] row = line.split(",");
+            for (int i = 0; i< row.length; i++) {
+                board[counter][i] = Integer.parseInt(row[i]);
+            }
+            counter++;
+        }
+
     }
 
     public int[][] getBoard() {
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,22 @@
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("Witaj Pawle :P");
+        FifteenPuzzle board = new FifteenPuzzle("unsolvedGame");
+        printBoard(board);
+    }
+
+    public static void printBoard(FifteenPuzzle board) {
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                if(board.getBoard()[i][j]/10 == 0) {
+                    System.out.print(board.getBoard()[i][j] + "  ");
+                } else {
+                    System.out.print(board.getBoard()[i][j] + " ");
+                }
+            }
+            System.out.println();
+        }
     }
 }
